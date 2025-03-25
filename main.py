41cc245a1552b3fad80f9f3da978d4642eb443e9
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-03-25 12:10:14 +0100

    basic

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import easygui
+
+
+
+
+letters = """SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%publIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A""" # ok
+echooff ="""@%publIc:~5,1%%pUBLIc:~0,1%ho oF%pUBLIc:~46,16%f""" # ok
+clss = """^%pUBlIC:~14,1%^L%pUBlic:~55,17%^%publIc:~4,1%""" # ok
+echoon = """@%pUBLIc:~5,1%%publIc:~0,1%ho O^n""" # ok
+
+def main():
+    print("main function")
+
+if __name__ == "__main__":
+    main()
